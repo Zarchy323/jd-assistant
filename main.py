c150821b6f602ab82e9a870bd9175f27ae3edd42
Zarchy y <yyc314391107@gmail.com>
2020-08-28 10:40:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     https://github.com/tychxn/jd-assistant/wiki/1.-%E4%BA%AC%E4%B8%9C%E6%8A%A2%E8%B4%AD%E5%8A%A9%E6%89%8B%E7%94%A8%E6%B3%95
     """
 
-    sku_ids = '100001324422'  # 商品id
+    sku_ids = '10020773660832'  # 商品id
     area = '1_72_4211'  # 区域id
     asst = Assistant()  # 初始化
     asst.login_by_QRcode()  # 扫码登陆
     asst.buy_item_in_stock(sku_ids=sku_ids, area=area, wait_all=False, stock_interval=5)  # 根据商品是否有货自动下单
     # 6个参数：
-    # sku_ids: 商品id。可以设置多个商品，也可以带数量，如：'1234' 或 '1234,5678' 或 '1234:2' 或 '1234:2,5678:3'
+    # sku_ids:10020773660832。可以设置多个商品，也可以带数量，如：'1234' 或 '1234,5678' 或 '1234:2' 或 '1234:2,5678:3'
     # area: 地区id
     # wait_all: 是否等所有商品都有货才一起下单，可选参数，默认False
     # stock_interval: 查询库存时间间隔，可选参数，默认3秒
